d089892125c852f1d37bb65632497059ad66c399
ashley o <oneal.ashleyc@gmail.com>
2018-05-17 07:30:33 -0400

    #2

--- game.js
+++ game.js
@@ -1,45 +1,52 @@
-var wordList = ["car", "dog", "tiger", "sloth", "chicken"];
+var wordList = ["cat", "dog", "tiger", "sloth", "chicken"];
 var computerChoice = wordList[Math.floor(Math.random() * wordList.length)];
 console.log(computerChoice);
-
-var win;
+var guesses = 10;
+var win = 0;
 var guessedSoFar = [];
 var answerArr = [];
-var answerString;
+var answerString = [];
 
 for (i = 0; i < computerChoice.length; i++) {
     answerArr[i] = "-"; 
 }
 
 answerString = answerArr.join(" ");
-document.getElementById("answer").innerHTML = answerString;
+document.getElementById("answer").innerHTML = answerArr;
 
 
 document.onkeyup= function(event) {
     var userGuess = event.key;
     guessedSoFar[guessedSoFar.length] = userGuess
-    console.log(userGuess);
-
-    for (i = 0; i < computerChoice.length; i++) {
+    answerString = computerChoice.split("");
+    guesses--;
+    document.getElementById("guesses").innerHTML = "Guesses left: " + guesses;
+    
+    
+    for (var i = 0; i < computerChoice.length; i++) {
         if (userGuess === computerChoice[i]) {
-            userGuess == answerString;
-            console.log(answerString[i]);
-            document.getElementById("answer").innerhtml = "-" + answerString;
+            answerArr[i] = userGuess;
+            document.getElementById("answer").innerHTML = answerArr[i];
+            
         }
+    }    
+    
+    document.getElementById("answer").innerHTML = answerArr.join("");
+    console.log(answerString);
+    console.log(answerArr);
+    
+    
+    if (answerArr == answerString) {
+        win = win + 1;
+        alert("you win");
+        document.getElementById("wins").innerHTML = "wins: " + win;
     }
-
-          
-        
+    
+    if (guesses < 1) {
+        alert("you lose");
+    }
+    
+    
+    document.getElementById("guessed").innerHTML = "Guessed so far :" + guessedSoFar;         
 }
-
-
-
-
-
-
-        
-
-
-
-
 
